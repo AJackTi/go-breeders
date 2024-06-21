e36e3674816418551436fb3e7a392fae38afd78e
AJackTi <dtrong97vn@gmail.com>
2024-06-21 11:57:02 +0700

    add: setup route

---
+++ cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	mux.Get("/", app.ShowHome)
+
+	return mux
+}
