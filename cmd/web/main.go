014578e1291022e72c3737544c7e2af9b3637ddb
AJackTi <dtrong97vn@gmail.com>
2024-06-21 11:34:24 +0700

    feat: add app and package go-chi

---
+++ cmd/web/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = ":4000"
+
+type application struct{}
+
+func main() {
+	// app := application{}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World!")
+	})
+
+	fmt.Println("Starting web application on port", port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-breeders
 
 go 1.22.0
+
+require github.com/go-chi/chi/v5 v5.0.13 // indirect
