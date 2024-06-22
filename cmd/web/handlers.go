dacda3bbec6181e87a091e955089a012e9ce3b61
AJackTi <dtrong97vn@gmail.com>
2024-06-22 21:31:12 +0700

    feat: update abstract factory

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -32,3 +32,23 @@ func (app *application) CreateCatFromFactory(w http.ResponseWriter, r *http.Requ
 func (app *application) TestPatterns(w http.ResponseWriter, r *http.Request) {
 	app.render(w, "test.page.gohtml", nil)
 }
+
+func (app *application) CreateDogFromAbstractFactory(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+	dog, err := pets.NewPetFromAbstractFactory("dog")
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	_ = t.WriteJSON(w, http.StatusOK, dog)
+}
+
+func (app *application) CreateCatFromAbstractFactory(w http.ResponseWriter, r *http.Request) {
+	var t toolbox.Tools
+	cat, err := pets.NewPetFromAbstractFactory("cat")
+	if err != nil {
+		_ = t.ErrorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+	_ = t.WriteJSON(w, http.StatusOK, cat)
+}
